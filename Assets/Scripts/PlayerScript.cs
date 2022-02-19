d4348f3aa5849430eaebed3c8814a92344c69e86
Mücahit Aktürk <m.akturk6134@gmail.com>
2022-02-19 20:59:20 +0300

    --update

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,30 +5,38 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     Rigidbody rb;
-    MeshFilter meshFilter;
-
+    SkinnedMeshRenderer meshFilter;
+    Animator playerAnimator;
     [SerializeField] private Mesh capsuleMesh = null;
     public static bool isHide = false;
 
-
+    private float speed = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        meshFilter = GetComponent<MeshFilter>();
+        meshFilter = transform.GetChild(1)
+            .GetComponent<SkinnedMeshRenderer>();
+
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector3(horizontal * 10, 0, vertical * 10);
+        rb.velocity = new Vector3(horizontal * 10, 0, vertical * 10).normalized;
         rb.velocity = transform.TransformDirection(rb.velocity);
 
         float mouseX = Input.GetAxisRaw("Mouse X");
         float mouseY = Input.GetAxisRaw("Mouse Y");
 
-        transform.Rotate(new Vector3(0, mouseX * 5, 0));
+        transform.rotation = Quaternion.Euler(0,90,vertical);
+
+        float i = rb.velocity.z + rb.velocity.x;
+
+        playerAnimator.SetFloat("speedZ", Mathf.Abs(i));
+       // playerAnimator.SetFloat("speedZ", Mathf.Abs(vertical));
+
     }
 
     private void OnTriggerStay(Collider other)
@@ -38,13 +46,13 @@ public class PlayerScript : MonoBehaviour
             if (Input.GetKey(KeyCode.G))
             {
                 MeshFilter otherMeshFilter = other.gameObject.GetComponent<MeshFilter>();
-                meshFilter.mesh = otherMeshFilter.mesh;
+                meshFilter.sharedMesh = otherMeshFilter.mesh;
                 rb.velocity = Vector3.zero;
                 isHide = true;
             }
             else
             {
-                meshFilter.mesh = capsuleMesh;
+                meshFilter.sharedMesh = capsuleMesh;
                 isHide = false;
             }
         }
@@ -63,7 +71,7 @@ public class PlayerScript : MonoBehaviour
     {
         if (other.gameObject.layer == 6)
         {
-            meshFilter.mesh = capsuleMesh;
+            meshFilter.sharedMesh = capsuleMesh;
         }
     }
 }
